bfdaeee539bfb3f605657b48e125ab96393881e1
jaan18 <jovas28@hotmail.com>
2019-07-10 23:29:43 -0500

    calcu

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+const numero1 = 2;
+const numero2 = 3;
+
+function sumar(a, b) {
+  return a + b;
+}
+
+function restar(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+
+console.log("Suma", sumar(numero1, numero2));
+console.log("Resta", restar(numero1, numero2));
+console.log("Multiplicacion", multiplicar(numero1, numero2));
+console.log("Division", dividir(numero1, numero2));
